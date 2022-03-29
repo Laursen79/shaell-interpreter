40e7bd96b6276439b5cf8a15c6a90da177a1ce37
Laursen79 <69715502+Laursen79@users.noreply.github.com>
2022-03-29 13:03:59 +0200

    Started implementing Job objects and external process calls. (#23)

---
+++ ShaellLang/JobObject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ShaellLang;
+
+public class JobObject : NativeTable, IValue
+{
+    private IValue Value
+    {
+        get => GetValue(new SString("value"));
+        set => SetValue("value", value);
+    }
+
+    public StreamReader In { get; set; }
+    public StreamWriter Out { get; set; }
+    public StreamReader Error { get; set; }
+
+    private Task<int> _process;
+
+    public JobObject(IValue value)
+    {
+        SetValue("value", value);
+    }
+
+    private JobObject(){}
+
+    public static class Factory
+    {
+        public static JobObject ProcessCall(Func<ICollection<IValue>, int> process, ICollection<IValue> args)
+        {
+            var func = new Func<object, int>(values => process.Invoke(values as ICollection<IValue>));
+            var jo = new JobObject
+            {
+                _process = Task.Factory.StartNew(func, args)
+            };
+            jo.Value = new Number(jo._process.Result);
+            
+            return jo;
+        }
+        public static JobObject ProcessCall(Func<int> process)
+        {
+            var jo = new JobObject
+            {
+                _process = Task.Factory.StartNew(process)
+            };
+            jo.Value = new Number(jo._process.Result);
+            
+            return jo;
+        }
+        
+    }
+
+    public bool ToBool()
+    {
+        return Value.ToBool();
+    }
+
+    public Number ToNumber()
+    {
+        return Value.ToNumber();
+    }
+
+    public IFunction ToFunction()
+    {
+        return Value.ToFunction();
+    }
+
+    public SString ToSString()
+    {
+        return Value.ToSString();
+    }
+
+    public ITable ToTable()
+    {
+        return this;
+    }
+}
---
+++ ShaellLang/SProcess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShaellLang;
+
+public class SProcess : IFunction
+{
+    public bool ToBool()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Number ToNumber()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IFunction ToFunction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public SString ToSString()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public ITable ToTable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IValue Call(ICollection<IValue> args)
+    {
+        var jo = JobObject.Factory.ProcessCall(() =>
+        {
+            int x = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                x += i + 3;
+            }
+
+            return x;
+        });
+        return jo;
+    }
+
+    public uint ArgumentCount { get; }
+}
