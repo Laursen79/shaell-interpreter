411c0008e4ae00db1cc9cc6d6a03ad846e4b405b
Simon Laursen <69715502+Laursen79@users.noreply.github.com>
2022-04-13 14:49:45 +0200

    Implemented error reporter

---
+++ ShaellLang/ShaellErrorReporter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Antlr4.Runtime;
+
+namespace ShaellLang;
+
+public class ShaellErrorReporter
+{
+    private List<ShaellError> errors = new List<ShaellError>();
+    public void ReportError(ShaellError e)
+    {
+        errors.Add(e);
+    }
+    
+    public bool HasErrors => errors.Count > 0;
+    
+    
+    public override string ToString()
+    {
+        string result = "";
+        foreach (ShaellError e in errors)
+        {
+            result += e.Message + "\n";
+        }
+        return result;
+    }
+
+    public void SetErrorListener(ShaellLexer lexer)
+    {
+        lexer.AddErrorListener(new ShaellLexerErrorListener(this));
+    }
+    
+    public void SetErrorListener(ShaellParser parser)
+    {
+        parser.AddErrorListener(new ShaellParserErrorListener(this));
+    }
+    
+}
+
+public struct ShaellError
+{
+    public string Message;
+    public Exception Exception;
+
+    public ShaellError([NotNull] string message, Exception exception)
+    {
+        Message = message;
+        Exception = exception;
+    }
+}
